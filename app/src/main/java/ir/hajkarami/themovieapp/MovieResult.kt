60435c5969524c7223a18a658c9e5c2d25a4bbf4
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-11 09:47:06 +0330

    add Movie ModelClass and MovieResult class

---
+++ app/src/main/java/ir/hajkarami/themovieapp/MovieResult.kt
@@ -0,0 +1,10 @@
+package ir.hajkarami.themovieapp
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResult(
+    val page: Int,
+    @SerializedName("results") val movies: List<Movie>,
+    @SerializedName("total_pages") val totalPages: Int,
+    @SerializedName("total_results") val totalResults: Int
+)
